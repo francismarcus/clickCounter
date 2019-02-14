de6a987cc1eeb7ed077b4821fcc27105f2379f8c
Marcus Francis <marcusmagnusson@outlook.com>
2019-02-14 14:43:32 +0100

    styled component

--- src/count.js
+++ src/count.js
@@ -1,8 +1,47 @@
 import React from 'react';
-import { throws } from 'assert';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 4em;
+  background: floralwhite;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+`;
+
+const Title = styled.h1`
+  font-size: 3em;
+  text-align: center;
+  color: #907163;
+`;
+
+const RedButton = styled.button`
+color: white;
+cursor: pointer;
+background-color: #e34234;
+line-height: normal;
+margin: 1% 1% 1% 1%;
+padding: 0.5% 1% 0.5% 1%;
+font-weight: 100;
+border: transparent;
+border-radius: 3px;
+font-size: 16px;
+text-decoration: one;
+`
+const BlackButton = styled.button`
+color: white;
+cursor: pointer;
+background-color: black;
+line-height: normal;
+margin: 1% 1% 1% 1%;
+padding: 0.5% 1% 0.5% 1%;
+font-weight: 100;
+border: transparent;
+border-radius: 3px;
+font-size: 16px;
+text-decoration: one;
+`
 
 class Count extends React.Component {
-
   constructor(props) {
     super(props);
 
@@ -14,22 +53,22 @@ class Count extends React.Component {
   increment = (n) => {
     this.setState({ count: this.state.count + n });
   }
-  
+
   decrement = (n) => {
     this.setState({ count: this.state.count - n });
   }
 
   render() {
     return (
-      <div>
-      <h1>The counter is currently {this.state.count}</h1>
-      <button onClick={() => this.decrement(1)}>
+      <Wrapper>
+      <Title>The counter is currently {this.state.count}</Title>
+      <BlackButton onClick={() => this.decrement(1)}>
         -
-      </button>
-      <button onClick={() => this.increment(1)}>
+      </BlackButton>
+      <RedButton onClick={() => this.increment(1)}>
         +
-      </button>
-      </div>
+      </RedButton>
+      </Wrapper>
     )
   }
 }
