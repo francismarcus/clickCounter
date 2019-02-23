ac551f984a94168e6843d8dd48c1ae8b8e240d98
Marcus Francis <marcusmagnusson@outlook.com>
2019-02-24 00:44:15 +0100

    dry tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,24 +7,34 @@ import Count from './count';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+const Attr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
 it('renders without error', () => {
   const wrapper = shallow(<App />);
-   expect(wrapper.find("[data-test='component-app']").length).toEqual(1);
+   expect(Attr(wrapper, 'component-app').length).toEqual(1);
 });
 
 it('renders inc button', () => {
   const wrapper = shallow(<Count />);
-   expect(wrapper.find("[data-test='inc-button']").length).toEqual(1);
+   expect(Attr(wrapper, 'inc-button').length).toEqual(1);
 });
 
 it('renders decrement button', () => {
   const wrapper = shallow(<Count />);
-   expect(wrapper.find("[data-test='dec-button']").length).toEqual(1);
+   expect(Attr(wrapper, 'dec-button').length).toEqual(1);
 });
 
 it('displays counter', () => {
   const wrapper = shallow(<Count />);
-   expect(wrapper.find("[data-test='counter-display']").length).toEqual(1);
+   expect(Attr(wrapper, 'counter-display').length).toEqual(1);
 });
 
 it('initial state is 0', () => {
